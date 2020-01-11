ca69f5221d293a6d851e9a4c8034a7bef30bbe23
frank400 <j.victorhuguenin2018@gmail.com>
2020-01-11 03:22:35 -0300

    python script to delete a table

---
+++ delete_table.py
@@ -0,0 +1,9 @@
+from db import new_connection
+from mysql.connector import ProgrammingError
+
+with new_connection() as connection:
+    try:
+        cursor = connection.cursor()
+        cursor.execute('DROP TABLE emails')
+    except ProgrammingError as e:
+        print(f'ERROR: {e}')
