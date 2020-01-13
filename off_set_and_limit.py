90130a68015483a1e61cee6172288f1208810771
frank400 <j.victorhuguenin2018@gmail.com>
2020-01-13 02:59:37 -0300

    a simple exemple of OFFSET and LIMIT on python

---
+++ off_set_and_limit.py
@@ -0,0 +1,17 @@
+from mysql.connector.errors import ProgrammingError
+from db import new_connection
+
+sql = 'SELECT*FROM contatos LIMIT %s OFFSET %s'
+args = (int(input('how many items do you want: ')),
+        int(input('set the offset: ')))
+
+with new_connection() as connection:
+    try:
+        cursor = connection.cursor()
+        cursor.execute(sql, args)
+        contatos = cursor.fetchall()
+    except ProgrammingError as e:
+        print(f'ERROR: {e.msg}')
+    else:
+        for contato in contatos:
+            print(f'{contato[2]:2d} - {contato[0]:10s} - Fone : {contato[1]:}')
